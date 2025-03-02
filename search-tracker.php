d2d9addd9c96c1977b46b5b60f448c223932803a
Marko Krstic <mailme@markokrstic.com>
2025-03-02 18:21:22 +0100

    1.0.0

--- search-tracker.php
+++ search-tracker.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Plugin Name: Search Query Tracker
- * Description: Tracks search queries and user clicks on search results.
- * Version: 1.0
- * Author: Your Name
+ * Plugin Name:       Search Query Tracker
+ * Description:       Search Query Tracker's plugin description
+ * Requires at least: 6.3.0
+ * Requires PHP:      7.4
+ * Version:           1.0.0
+ * Author:            devusrmk
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       search_query_tracker
+ * Website:           
  */
 
 // Exit if accessed directly
@@ -18,3 +24,30 @@ define('SQT_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 // Load the main application file
 require_once SQT_PLUGIN_DIR . 'app/app.php';
+
+
+// ------------------------------------------------------------------------------------------------
+// Self Hosted Plugin Updater
+// ------------------------------------------------------------------------------------------------
+
+$plugin_prefix = 'SEARCHQUERYTRACKER';
+
+// Extract the version number
+$plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
+
+// Plugin Constants
+define($plugin_prefix . '_DIR', plugin_basename(__DIR__));
+define($plugin_prefix . '_BASE', plugin_basename(__FILE__));
+define($plugin_prefix . '_PATH', plugin_dir_path(__FILE__));
+define($plugin_prefix . '_VER', $plugin_data['Version']);
+define($plugin_prefix . '_CACHE_KEY', 'search_query_tracker-cache-key-for-plugin');
+define($plugin_prefix . '_REMOTE_URL', 'https://selfhost.dplugins.com/wp-content/uploads/downloads/7/info.json');
+
+require constant($plugin_prefix . '_PATH') . 'inc/update.php';
+
+new DPUpdateChecker(
+	constant($plugin_prefix . '_BASE'),
+	constant($plugin_prefix . '_VER'),
+	constant($plugin_prefix . '_CACHE_KEY'),
+	constant($plugin_prefix . '_REMOTE_URL'),
+);
